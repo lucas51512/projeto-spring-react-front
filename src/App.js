5cb35448ffafc0e7f881c23bdca92b4dc2a2dabf
lucas51512 <lucasbrancalhao454@gmail.com>
2023-02-15 10:23:56 -0300

    Front parcialmente finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
 
 function App() {
+
+  const produto = {
+    codigo : 0,
+    nome: '',
+    marca: ''
+  }
+
+  // UseState
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
+
+  //useEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+  //Obtendo os dados do formulário
+  const aoDigitar = (e) => {
+    setObjProduto({...objProduto, [e.target.name]:e.target.value})
+  }
+
+  //Cadastrar produto
+  const cadastrar = () => {
+      fetch("http://localhost:8080/cadastrar",{
+        method:'post',
+        body:JSON.stringify(objProduto),
+        headers:{
+          'Content-type':'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => {
+        console.log(retorno_convertido);
+        if(retorno_convertido.mensagem !== undefined){
+          alert(retorno_convertido.mensagem);
+        }else{
+          setProdutos([...produtos, retorno_convertido]);
+          alert('Produto cadastrado com sucesso!');
+          limparFormulario();
+        }
+      })
+  }
+
+  //Limpar formulário
+
+  const limparFormulario = () => {
+    setObjProduto(produto);
+    setBtnCadastrar(false);
+  }
+
+  //Selecionar produto
+  const selecionarProduto = (indice) => {
+    setObjProduto(produtos[indice]);
+    setBtnCadastrar(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formulario botao={btnCadastrar}
+       eventoTeclado={aoDigitar} cadastrar={cadastrar}
+        obj={objProduto} cancelar={limparFormulario}/>
+      <Tabela vetor={produtos} selecionar={selecionarProduto}/>
     </div>
   );
 }
